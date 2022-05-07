1c8d31fd5869abb85e67e1e88288f1cf1761d02f
adriculous <admarpad@gmail.com>
2022-05-07 15:14:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +15,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
